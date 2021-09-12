c5bb01deb199e24781473ee5b8f46e42a657f189
acer_laptop <cjong93.login@gmail.com>
2021-09-12 11:45:08 -0400

    Initialized scrapy pipelines

--- scrotten_crawler/scrotten_crawler/pipelines.py
+++ scrotten_crawler/scrotten_crawler/pipelines.py
@@ -5,7 +5,28 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from movie.models import Movie
+
+def clean_title(param):
+    return param
 
 class ScrottenCrawlerPipeline(object):
+
     def process_item(self, item, spider):
+        movie_id = clean_title(item['movie_id'])
+        movie_id = clean_title(item['movie_id'])
+        movie_id = clean_title(item['movie_id'])
+        movie_id = clean_title(item['movie_id'])
+        movie_id = clean_title(item['movie_id'])
+
+    # movie_id = models.CharField(max_length=255, unique=True)
+    # name = models.CharField(max_length=255)
+    # genre = models.CharField(max_length=255)
+    # year = models.IntegerField(max_length=4)
+    # gross_sales = models.BigIntegerField()
+
+        Movie.objects.create(
+            title=title,
+        )
+
         return item
