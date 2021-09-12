aaa3f74dca62d2c9986917db3f4b0f7cc8363a93
acer_laptop <cjong93.login@gmail.com>
2021-09-12 18:34:29 -0400

    Added information on the data that will be retrieved

---
+++ config.py
@@ -0,0 +1,5 @@
+rotten_link_prefix = dict(
+    movie = 'https://www.rottentomatoes.com/m/',
+    tv = 'https://www.rottentomatoes.com/tv',
+    member = 'https://www.rottentomatoes.com/celebrity/',
+)
