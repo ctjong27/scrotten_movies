ba173941fc00be5ebb103bb068e527bb6b947fe4
acer_laptop <cjong93.login@gmail.com>
2021-09-12 15:25:12 -0400

    updated "scrotten_analysis" to "app"

--- scrotten_crawler/settings.py
+++ scrotten_crawler/settings.py
@@ -3,7 +3,7 @@
 import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), ".."))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'scrotten_analysis.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'app.settings'
 import django
 django.setup()
 
--- scrotten_movie/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class ScrottenMovieConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'scrotten_movie'
