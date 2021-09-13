ce0b3cfa4d8e432d14ce85f4d93beb3f2b07530c
acer_laptop <cjong93.login@gmail.com>
2021-09-12 20:05:48 -0400

    updated model id to be PK

--- scrotten_movie/models.py
+++ scrotten_movie/models.py
@@ -1,11 +1,12 @@
+import uuid
 from django.db import models
 
 class ScrottenMovie(models.Model):
-    movie_id = models.CharField('Movie Id',max_length=255, unique=True)
-    name = models.CharField('Name',max_length=255, unique=True)
-    genre = models.CharField('Genre',max_length=255, unique=True)
-    year = models.CharField('Year',max_length=255, unique=True)
-    gross_sales = models.CharField('Gross Sales',max_length=255, unique=True)
+    movie_id = models.CharField('Movie Id', primary_key=True, max_length=255, unique=True)
+    name = models.CharField('Name', max_length=255, unique=True)
+    genre = models.CharField('Genre', max_length=255, unique=True)
+    year = models.CharField('Year', max_length=255, unique=True)
+    gross_sales = models.CharField('Gross Sales', max_length=255, unique=True)
     
     def __str__(self):
         return self.title
