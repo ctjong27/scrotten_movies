8416c0706b567bae8feaba32ecb3d515e6ea498d
cjong mac1 <cjong93.login@gmail.com>
2022-01-24 17:01:01 -0500

    correction for indexing table model

--- scrotten_movie_to_member/models.py
+++ scrotten_movie_to_member/models.py
@@ -5,8 +5,8 @@ class ScrottenMovieToMember(models.Model):
     
     id = models.CharField('Id', primary_key=True, max_length=255, unique=True)
     
-    movie_id = models.CharField('Movie Id', max_length=255, unique=True)
-    member_id = models.CharField('Member Id', max_length=255, unique=True)
+    movie_id = models.CharField('Movie Id', max_length=255, unique=False)
+    member_id = models.CharField('Member Id', max_length=255, unique=False)
     
     def __str__(self):
         return self.id
