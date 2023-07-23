1b01650be966ed5df3671b654f220903b55cfb9c
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2023-07-23 19:33:19 -0400

    scraping show-to-person

--- code/scrape_top_medium/scripts/tv_show_scraper.py
+++ code/scrape_top_medium/scripts/tv_show_scraper.py
@@ -2,57 +2,74 @@ import requests
 import csv
 import os
 import pathlib
+from tqdm import tqdm
 
 # Variables
 num_top_shows = 25  # Desired number of top shows here
 
-# # Determine root directory
-# root_dir = pathlib.Path(__file__).parent.absolute()
-
-# # Read API key from the root directory
-# with open(os.path.join(root_dir, '..', 'api_key.txt'), 'r') as file:
-# # with open(os.path.join(root_dir, 'api_key.txt'), 'r') as file:
-#     api_key = file.read().strip()
-
-
 # Get current working directory
 cwd = os.getcwd()
 
-# Create data directory if it does not exist
-os.makedirs(os.path.join(cwd, 'data'), exist_ok=True)
+# File path for the data
+file_path = os.path.join(cwd, f'data/tv_shows_top_{num_top_shows}_yearly.csv')
 
-with open(os.path.join(cwd, 'api_key.txt'), 'r') as file:
-    api_key = file.read().strip()
+# Check if file already exists
+if os.path.exists(file_path):
+    print("File already exists. Skipping...")
+else:
+    # Create data directory if it does not exist
+    os.makedirs(os.path.join(cwd, 'data'), exist_ok=True)
 
-# Prepare data for CSV
-csv_data = []
+    with open(os.path.join(cwd, 'api_key.txt'), 'r') as file:
+        api_key = file.read().strip()
 
-for year in range(1970, 2021):
-    page = 1
-    shows_filtered = []
-    
-    while len(shows_filtered) < num_top_shows:
-        url = f"https://api.themoviedb.org/3/discover/tv?api_key={api_key}&language=en-US&sort_by=popularity.desc&first_air_date_year={year}&region=US&page={page}"
+    # Prepare data for CSV
+    csv_data = []
 
-        response = requests.get(url)
+    # Initialize progress bar
+    pbar = tqdm(total=(2021-1970+1)*num_top_shows)
 
-        if response.status_code == 200:
-            data = response.json()
-            shows = data['results']
+    for year in range(1970, 2021):
+        page = 1
+        shows_filtered = []
+
+        while len(shows_filtered) < num_top_shows:
+            url = f"https://api.themoviedb.org/3/discover/tv?api_key={api_key}&language=en-US&sort_by=popularity.desc&first_air_date_year={year}&region=US&page={page}"
+
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                data = response.json()
+                shows = data['results']
+
+                # Filter shows based on original_language and origin_country
+                shows_filtered += [show for show in shows if show['original_language'] == 'en' and 'US' in show['origin_country']]
+
+                page += 1
+            else:
+                print("Error:", response.status_code)
+                break
+
+        for i, show in enumerate(shows_filtered[:num_top_shows], start=1):
+            csv_data.append([year,
+                            # i,
+                            show['name'],
+                            show['id'],
+                            ", ".join([str(id) for id in show['genre_ids']]), show['first_air_date'], 
+                            ", ".join(show['origin_country']), 
+                            show['vote_average'], 
+                            show['popularity']])
             
-            # Filter shows based on original_language and origin_country
-            shows_filtered += [show for show in shows if show['original_language'] == 'en' and 'US' in show['origin_country']]
+            # Update progress bar
+            pbar.update(1)
 
-            page += 1
-        else:
-            print("Error:", response.status_code)
-            break
+    # Close progress bar
+    pbar.close()
 
-    for i, show in enumerate(shows_filtered[:num_top_shows], start=1):
-        csv_data.append([year, i, show['name'], ", ".join([str(id) for id in show['genre_ids']]), show['first_air_date'], ", ".join(show['origin_country']), show['vote_average'], show['popularity']])
-
-# Write to CSV in the data directory in the root directory
-with open(os.path.join(cwd, f'data/tv_shows_top_{num_top_shows}_yearly.csv'), 'w', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f)
-    writer.writerow(["Year", "Rank", "Show Name", "Genre IDs", "First Air Date", "Origin Country", "Rating", "Popularity"])  # Write header
-    writer.writerows(csv_data)  # Write data
+    # Write to CSV in the data directory in the root directory
+    with open(file_path, 'w', newline='', encoding='utf-8') as f:
+        writer = csv.writer(f)
+        # writer.writerow(["Year", "Rank", "Show Name", "Show ID", "Genre IDs", "First Air Date", "Origin Country", "Rating", "Popularity"])  # Write header
+        # writer.writerow(["Year", "Show Name", "Show ID", "Genre IDs", "First Air Date", "Origin Country", "Rating", "Popularity"])  # Write header
+        writer.writerow(["year", "name", "id", "genres", "first_air_date", "origin_country", "rating", "popularity"])  # Write header
+        writer.writerows(csv_data)  # Write data
