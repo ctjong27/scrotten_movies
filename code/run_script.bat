3e9956138e8e6879ecadeffc29531f6c888900b4
TheDayDreamer <40523532+Fractate@users.noreply.github.com>
2023-07-26 00:03:14 -0400

    data export complete

--- code/run_script.bat
+++ code/run_script.bat
@@ -5,6 +5,9 @@ REM Check if diagnostic flag is present
 if "%~1"=="-d" (
     echo Running diagnostic script...
     python ./scripts/diagnostic_script.py
+) else if "%~1"=="-e" (
+    echo Running final data cleanup and export
+    python ./scripts/tv_data_final_prep.py
 ) else (
     echo Running scripts...
     python ./scripts/tv_show_scraper.py
